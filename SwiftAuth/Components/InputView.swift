3f6abeb73cd2aa0ea6e6781875f63692dc185a4e
martin-ngigi <martinwainaina001@gmail.com>
2023-11-26 16:53:42 +0300

    Login UI done

---
+++ SwiftAuth/Components/InputView.swift
@@ -0,0 +1,41 @@
+//
+//  InputView.swift
+//  SwiftAuth
+//
+//  Created by Martin Wainaina on 26/11/2023.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    @Binding var text: String
+    var title: String
+    var placeHolder: String
+    var isSecuredField = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12){
+            Text(title)
+                .foregroundStyle(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSecuredField {
+                SecureField(placeHolder, text: $text)
+                    .font(.system(size: 14))
+            }
+            else{
+                TextField(placeHolder, text: $text)
+                    .font(.system(size: 14))
+            }
+            
+            Divider()
+        }
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView(text: .constant(""), title: "Email Address", placeHolder: "name@example.com")
+    }
+}
---
+++ SwiftAuth/Core/Authentication/LoginView.swift
@@ -0,0 +1,93 @@
+//
+//  LoginView.swift
+//  SwiftAuth
+//
+//  Created by Martin Wainaina on 26/11/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                // Image
+                Image("firebase-logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+                
+                // Form fields
+                VStack{
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeHolder: "name@example.com")
+                        .autocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeHolder: "Enter your password",
+                              isSecuredField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                // Forgot password
+                NavigationLink{
+                    
+                } label: {
+                    HStack(){
+                        Spacer()
+                        Text("Forgot password?")
+                            .foregroundColor(.blue)
+                            .underline()
+                    }
+                    .padding()
+                }
+                
+                
+                
+                // Sign in button
+                Button{
+                    
+                } label: {
+                    HStack{
+                        Text("SignIn")
+                            .fontWeight(.semibold)
+                        
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(15)
+                .padding(.top, 24)
+                
+                Spacer()
+                
+                // Signup button
+                NavigationLink{
+                    
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Don't have an account? ")
+                        Text("SignUp")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 17))
+                }
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
