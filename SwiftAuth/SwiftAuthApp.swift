de74bc31c39b3cf3bad86864ac259535ed174b24
martin-ngigi <martinwainaina001@gmail.com>
2023-11-26 16:10:11 +0300

    Initial commit

---
+++ SwiftAuth/SwiftAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAuthApp.swift
+//  SwiftAuth
+//
+//  Created by Martin Wainaina on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
